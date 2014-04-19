9b2f677f738d51c12976e158fbf99286cb5384cf
phantarch <riley.turben@gmail.com>
2014-04-18 20:38:39 -0500

    Base server code

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.use(express.logger('dev'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/blarg', function(req, res){
+  res.send('Hello World');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
