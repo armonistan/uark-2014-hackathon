687b9d8b13a32ad512d846bb8c29492d8413fc38
phantarch <riley.turben@gmail.com>
2014-04-18 20:59:20 -0500

    Basic jade templates

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res){
+	res.render('index',
+	{title : "hackathon"}
+	);
+});
+
 app.get('/blarg', function(req, res){
   res.send('Hello World');
 });
