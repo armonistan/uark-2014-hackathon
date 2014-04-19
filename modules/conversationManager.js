9139d82776581f902acf529b14728791d7e04686
dpumford <dpumford@hotmail.com>
2014-04-19 11:15:18 -0500

    -Finished off backend for session creation.

--- modules/conversationManager.js
+++ modules/conversationManager.js
@@ -4,13 +4,17 @@ exports.validateUsers = function(user1, user2, f) {
 	//Make sure the users are real
 	var numUsers = 0;
 	
-	fs.readdir('users', function(error, files) {
-		for(var i = 0; i < files.length; i++) {
-			if (files[i] == user1 + '.json' || files[i] == user2 + '.json') {
-				numUsers++;
-			}
+	console.log(user1);
+	
+	var files = fs.readdirSync('users');
+	
+	for(var i = 0; i < files.length; i++) {
+		if (files[i] == user1 + '.json' || files[i] == user2 + '.json') {
+			numUsers++;
+			
+			console.log("Found" + files[i] + numUsers);
 		}
-	});
+	}
 	
 	if (numUsers == 2) {
 		//The users exist
@@ -23,14 +27,12 @@ exports.validateUsers = function(user1, user2, f) {
 
 exports.createConversation = function(user1, user2) {
 	var numConversations;
-	fs.readdir('convos', function(error, files) {
-		numConversations = files.length;
-	});
+	var files = fs.readdirSync('convos');
 	
-	var convoNumber = numConversations + 1;
+	var convoNumber = files.length + 1;
 	
 	fs.writeFile('convos/' + convoNumber + '.json',
-		'{"user1": "'+ user1 +'", "user2": "' + user2 + '", "messages": []}',
+		'{"users": ["' + user1 +'", "' + user2 + '"], "messages": []}',
 		function(error){
 			if(error) {
 				throw error;
@@ -47,8 +49,6 @@ exports.loadConversation = function(number) {
 	var convoFile;
 	
 	convoFile = JSON.parse(fs.readFileSync('convos/' + number + '.json'));
-	
-	console.log(convoFile);
-	
+
 	return convoFile;
 }
