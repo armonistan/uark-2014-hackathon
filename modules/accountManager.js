a0282220a94a5332e92bdd0b8b1ca300fba55f18
phantarch <riley.turben@gmail.com>
2014-04-18 23:09:32 -0500

    Account validator

---
+++ modules/accountManager.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+
+exports.validateSignup = function(name) {
+	fs.readaddr('../users', function(error, files) {
+		for(i = 0; i < files.length; i++) {
+			if (files[i] == name + '.json') {
+				return false;
+			}
+		}
+	});
+	return true;
+}
+
+exports.validateLogin = function(name, password) {
+	fs.readaddr('../users', function(error, files) {
+		for(i = 0; i < files.length; i++) {
+			if (files[i] == name + '.json') {
+				fs.readFile('../users' + files[i], function(error, data){
+					if (error) throw error;
+
+					if(JSON.parse(data).pass == password) {
+						return true;
+					} else {
+						return false;
+					}
+				});
+			}
+		}
+	});
+	return false;
+}
+
+exports.createUser = function(name, password) {
+	fs.writeFile('../users', '{name: '+ name +', pass: ' + password + '}', function(error){
+		if(error) throw error;
+
+		console.log("created a new user " + name);
+	}
+}
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+
+var AM = require('./modules/accountManager');
+
 app.use(express.logger('dev'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -12,6 +15,17 @@ app.get('/', function(req, res){
 	);
 });
 
+app.post('/', function(req, res){
+	if (AM.validateLogin(req.param('name'), req.param('pass')) {
+		req.session.loggedIn = true;
+		req.session.name = req.param('name');
+		res.redirect('/landing/' + req.param('name'));
+	} else {
+		res.render("Incorrect login information!");
+		setTimeout(function(){res.redirect('/')}, 300);
+	}
+});
+
 app.get('/help', function(req, res){
 	res.render('help',
 	{title : "Help with Pork!"}
@@ -22,6 +36,35 @@ app.get('/about', function(req, res){
 	res.render('about',
 	{title : "About Pork!"}
 	);
+});
+
+app.get('/signup', function(req, res){
+	res.render('signup',
+	{title : "Signup for Pork!"}
+	);
+});
+
+app.post('/signup', function(req, res){
+	if (AM.validateSignup(req.param('name')) {
+		AM.createUser(req.param('name'), req.param('pass'));
+		req.session.loggedIn = true;
+		req.session.name = req.param('name');
+		res.redirect('/landing/' + req.param('name'));
+	} else {
+		res.render("Account already in use!");
+		setTimeout(function(){res.redirect('/')}, 300);
+	}
+});
+
+app.get('/landing/:name', function(req, res){
+	if(req.session.loggedIn && req.session.name == req.params.name){
+		res.render('landing',
+			{name : req.params.name}
+		);
+	} else {
+		res.render("This is not your homepage!");
+		setTimeout(function(){res.redirect('/')}, 300);
+	}
 });
 
 var server = app.listen(3000, function() {
